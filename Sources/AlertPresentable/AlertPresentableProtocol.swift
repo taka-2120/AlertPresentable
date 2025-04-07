571ffab7f1b768ee8b4938d6639d6e960c3faf73
taka-2120 <yu_tkhs@icloud.com>
2025-04-07 21:53:21 +0900

    Remove header comments

--- Sources/AlertPresentable/AlertPresentableProtocol.swift
+++ Sources/AlertPresentable/AlertPresentableProtocol.swift
@@ -1,10 +1,3 @@
-//
-//  AlertPresentableProtocol.swift
-//  FTPresentation
-//
-//  Created by Yu Takahashi on 12/9/24.
-//
-
 ///
 /// `AlertPresentable` protocol is required when you use `alert(using: AlertPresentable)` modifier
 /// This protocol includes `alertController` variable to configure all necessary structs and values.
