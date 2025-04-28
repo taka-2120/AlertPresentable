f22f2eb6e09f850355061680c11c803a7b576339
taka-2120 <yu_tkhs@icloud.com>
2025-04-28 23:09:00 +0900

    Add MainActor attribute to AlertPresentable protocol

--- Sources/AlertPresentable/AlertPresentableProtocol.swift
+++ Sources/AlertPresentable/AlertPresentableProtocol.swift
@@ -2,6 +2,7 @@
 /// `AlertPresentable` protocol is required when you use `alert(using: AlertPresentable)` modifier
 /// This protocol includes `alertController` variable to configure all necessary structs and values.
 ///
+@MainActor
 public protocol AlertPresentable {
     ///
     /// `AlertController` includes functions to show, necessary variables, and structs which configure alert's actions and mode.
